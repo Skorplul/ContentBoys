9a08ea11fa1c3d5d08219a0323ff883a9238a458
Skorp <105490480+Skorplul@users.noreply.github.com>
2024-12-26 02:18:00 +0100

    added infinit sprint, and configs, Configs not yet accessable or modifiable without recomppile

---
+++ Configs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentBoys
+{
+    public class Configs
+    {
+        public static bool infinitSprint {  get; set; } = false;
+
+        public static bool shopIsFree { get; set; } = false;
+    }
+}
--- Hooks/ExampleShoppingCartPatch.cs
+++ Hooks/ExampleShoppingCartPatch.cs
@@ -1,5 +1,6 @@
 using System;
 using Photon.Pun;
+using UnityEngine;
 
 namespace ContentBoys.Patches
 {
@@ -15,6 +16,7 @@ namespace ContentBoys.Patches
 
             On.ShoppingCart.AddItemToCart += ShoppingCart_AddItemToCart;
             On.ShopHandler.OnAddToCartItemClicked += ShopHandler_OnAddToCartClicked;
+            On.PlayerController.Update += PlayerController_Update;
         }
 
         private static void ShoppingCart_AddItemToCart(On.ShoppingCart.orig_AddItemToCart orig, ShoppingCart self, ShopItem item)
@@ -32,20 +34,33 @@ namespace ContentBoys.Patches
 
         private static void ShopHandler_OnAddToCartClicked(On.ShopHandler.orig_OnAddToCartItemClicked orig, ShopHandler self, byte itemID)
         {
+            Debug.Log("Item Clicked!");
             ShopItem shopItem = default(ShopItem);
             if (self.TryGetShopItem(itemID, ref shopItem))
             {
                 int cost = self.m_ShoppingCart.CartValue + shopItem.Price;
-                if (!self.m_RoomStats.CanAfford(cost))
+                if (!self.m_RoomStats.CanAfford(cost) && !Configs.shopIsFree)
                 {
-                    ContentBoys.Logger.LogDebug("not enough money");
+                    self.noMoneySFX.Play(self.transform.position, false, 1f, null);
+                    Debug.Log("Cant afford, dont add to cart");
+                    return;
+                }
+                else if (PhotonNetwork.IsMasterClient)
+                {
+                    Debug.Log("Cant afford, adding anyway");
+                }
+                else if (!PhotonNetwork.IsMasterClient)
+                {
+                    self.noMoneySFX.Play(self.transform.position, false, 1f, null);
+                    Debug.Log("Not host, cant add to cart");
+                    return;
                 }
             }
             if (PhotonNetwork.IsMasterClient)
             {
                 self.m_PhotonView.RPC("RPCA_AddItemToCart", RpcTarget.All, new object[]
                 {
-                itemID
+                    itemID
                 });
                 self.addSFX.Play(self.transform.position, false, 1f, null);
                 return;
@@ -53,10 +68,57 @@ namespace ContentBoys.Patches
             self.addSFX.Play(self.transform.position, false, 1f, null);
             self.m_PhotonView.RPC("RPCM_RequestShopAction", RpcTarget.MasterClient, new object[]
             {
-            2,
-            itemID
+                2,
+                itemID
             });
-            ContentBoys.Logger.LogDebug("Added anyways");
+        }
+
+        private static void PlayerController_Update(On.PlayerController.orig_Update orig, PlayerController self)
+        {
+            if (self.player.data.dead)
+            {
+                return;
+            }
+            if (self.player.NoControl())
+            {
+                return;
+            }
+            if (self.player.data.isSprinting)
+            {
+                if (Configs.infinitSprint)
+                {
+                    self.player.data.currentStamina = 100;
+                }
+                else
+                {
+                    self.player.data.currentStamina = Mathf.MoveTowards(self.player.data.currentStamina, 0f, Time.deltaTime);
+                }
+                if (self.player.data.currentStamina < 0.01f)
+                {
+                    self.player.data.staminaDepleated = true;
+                }
+            }
+            else
+            {
+                if (self.player.data.sinceSprint > 1f)
+                {
+                    self.player.data.currentStamina = Mathf.MoveTowards(self.player.data.currentStamina, self.maxStamina, Time.deltaTime);
+                }
+                if (self.player.data.currentStamina >= self.staminaReActivationThreshold * 0.99f)
+                {
+                    self.player.data.staminaDepleated = false;
+                }
+            }
+            if (self.player.data.isLocal)
+            {
+                self.Look();
+                self.MovementStateChanges();
+                if (self.player.input.jumpWasPressed)
+                {
+                    self.TryJump();
+                }
+            }
+            self.SetRotations();
         }
     }
 }
